1469b658ab5e18992c8da54b2a3e4befb3c88b08
mjeon <mjeon@MERRY>
2025-02-19 10:58:58 +0900

    초기 커밋.

---
+++ 10798.cpp
@@ -0,0 +1,35 @@
+﻿#include <stdio.h>
+#include <string.h>
+#pragma warning (disable : 4996)
+
+int main(void)
+{
+	char str[5][16];
+
+	memset(str, '?', sizeof(str));
+
+	for (int i = 0; i < 5; i++)
+	{
+		str[i][15] = '\0';
+	}
+
+	for (int i = 0; i < 5; i++)
+	{
+		scanf("%s", str[i]);
+	}
+
+	for (int i = 0; i < 15; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			if (str[j][i] == '\0' || str[j][i] == '?')
+			{
+				continue;
+			}
+
+			printf("%c", str[j][i]);
+		}
+	}
+
+	return 0;
+}
